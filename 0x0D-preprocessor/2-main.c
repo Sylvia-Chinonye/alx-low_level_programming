19c14d7c809f0216eafedef7346c8e1d80cc580b
Nwabueze Sylvia <sylviachinonye21@gmail.com>
2023-04-18 11:34:24 -0700

    preprocessor task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints name of the file that its compiled from
+ * Description: a header file
+ *
+ * Return: always 0
+ */
+int main()
+{
+	printf("Filename: %s\n", _FILE_);
+	return (0);
+}
