ba16d1dc450cca85be2d4cf94544b7bcaaee86f5
Nwabueze Sylvia <sylviachinonye21@gmail.com>
2023-04-20 12:05:13 -0700

    added stdio.h

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,7 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdio.h>
+
 /**
  * sum_them_all - function that returns the sum of all its parameters
  * @n: the number of parameter passed to the function
